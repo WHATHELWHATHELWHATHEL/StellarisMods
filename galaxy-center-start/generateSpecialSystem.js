91be1af0bfd53e99e7aba4c517582e85511b0052
Yifan.chen <715027017@qq.com>
2018-03-11 20:33:07 +0800

    save current successful case in building the random galaxy with center start

--- galaxy-center-start/generateSpecialSystem.js
+++ galaxy-center-start/generateSpecialSystem.js
@@ -2,8 +2,8 @@ module.exports = {
   generateSpeicalSystems({ scenariosConfig: { specialSystems } }, indexSeed) {
     const baseIndex = indexSeed + 1;
     return specialSystems.map(
-      ({ name, initializer, position: { x, y } }, index) => (
-        `system = { id = \"${baseIndex + index}\" name = \"${name}\" initializer = ${initializer} position = { x = ${x} y = ${y}  }}`
+      ({ name, initializer, position: { x, y, z } }, index) => (
+        `system = { id = \"${baseIndex + index}\" name = \"${name}\" initializer = ${initializer} position = { x = ${x} y = ${y} z=${z} }}`
       )
     );
   },
