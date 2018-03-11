e22f71343639e06b17fe05e4d7797c6b48b8a815
Yifan.chen <715027017@qq.com>
2018-03-11 21:25:44 +0800

    save current progress with many good staff

--- galaxy-center-start/generate.js
+++ galaxy-center-start/generate.js
@@ -3,24 +3,28 @@ const config = require('./param.json');
 
 // get the functions to generate the random systems
 const generateRandomSystem = require('./generateRandomSystem').generateSysCoords;
-const randomSystems = generateRandomSystem(config);
 
 // then generate the given special systems
 const generateSpecialSystem = require('./generateSpecialSystem').generateSpeicalSystems;
-const specialSystem = generateSpecialSystem(config, randomSystems.length);
 
 // get the function that generate the final script code
 const generateStaticScenario = require('./generateFinalStaticScenario').generateFinalStaticScenrio;
-const finalScript = generateStaticScenario(config, 0, randomSystems.concat(specialSystem));
+
+// generate all the random scenes
+const { scenariosToCreate } = config;
+const finalScripts = 'a'.repeat(scenariosToCreate).split('').map((e, index) => {
+  // get random systems
+  const randomSystems = generateRandomSystem(config);
+  // then generate the special systems
+  const specialSystem = generateSpecialSystem(config, randomSystems.length);
+  return generateStaticScenario(config, index, randomSystems.concat(specialSystem));
+});
 
 // then output the result to the setup_scenarios
 const fs = require('fs');
-const dataBuffer = new Buffer(finalScript);
 
-const { scenariosToCreate } = config;
-
-for (let index = 0; index < 1; index += 1) {
-  const systems = randomSystems.concat(specialSystem);
+finalScripts.map((finalScript, index) => {
+  const dataBuffer = new Buffer(finalScript);
   fs.writeFile(
     `${__dirname}/map/setup_scenarios/test${index}.txt`,
     new Buffer(finalScript),
@@ -28,8 +32,8 @@ for (let index = 0; index < 1; index += 1) {
       if (error) {
         console.log(`write fail: ${JSON.stringify(error)}`);
       } else {
-        console.log('write success');
+        console.log(`${index} write success`);
       }
     },
   );
-}
+});
